a784dda9c6f10e848efdd83a7e77f073a7ac70b5
Caligo <schattenetcaligo@gmail.com>
2022-02-07 12:06:40 +0530

    ad stuff

--- pages/sauce/index.vue
+++ pages/sauce/index.vue
@@ -1,35 +1,44 @@
 <template>
   <div>
-    <div class="p-4 flex-col items-center flex" id="popunder" />
+    <div class="p-4 flex-col items-center flex"
+         id="popunder" />
 
-    <div class="p-4 flex-col items-center hidden md:flex" id="dada" />
-    <div class="p-4 flex flex-col items-center md:hidden" id="dadam" />
+    <div class="p-4 flex-col items-center hidden md:flex"
+         id="dada" />
+    <div class="p-4 flex flex-col items-center md:hidden"
+         id="dadam" />
 
-<div class = "p-4" id = "newm" />
-    <div
-      class="
+    <div class="
         max-w-md
         mx-auto
         bg-grayshit-500 bg-opacity-90
         shadow-md
         overflow-hidden
         md:max-w-2xl
-      "
-    >
+      ">
       <div class="p-4 border-red-500 border-2 flex flex-col items-center">
         <p class="font-semibold p-2 text-lg">{{ sauce.caption }}</p>
         <p class="font-semibold p-2 text-md">Scroll down for link</p>
-        <img class="object-cover w-full p-2" :src="sauce.image" />
-        <p class="font-semibold p-2 text-lg">Link: <a :href = "sauce.link">{{ sauce.link }}</a></p>
+        <img class="object-cover w-full p-2"
+             :src="sauce.image" />
+        <p class="font-semibold p-2 text-lg">Link: <a :href="sauce.link">{{ sauce.link }}</a></p>
       </div>
     </div>
-    <div class="p-4 flex-col items-center hidden md:flex" id="dada1" />
-    <div class="p-4 flex flex-col items-center md:hidden" id="dadam1" />
-   
-    <script
-      type="text/javascript"
-      src="https://groundlesscobwebmiller.com/bed640522658624db79a5a4634855709/invoke.js"
-    ></script>
+    <div class="p-4 flex-col items-center hidden md:flex"
+         id="dada1" />
+    <div class="p-4 flex flex-col items-center md:hidden"
+         id="dadam1" />
+    <div class="p-4"
+         id="newm" />
+    <div class="p-4"
+         id="newm1" />
+    <div class="p-4"
+         id="newm2" />
+    <div class="p-4"
+         id="newm3" />
+
+    <script type="text/javascript"
+            src="https://groundlesscobwebmiller.com/bed640522658624db79a5a4634855709/invoke.js"></script>
     <div id="container-bed640522658624db79a5a4634855709"></div>
   </div>
 </template>
@@ -53,7 +62,45 @@ export default {
       console.error(e)
       this.err = e
     }
-    postscribe(`#newm`, `<script data-cfasync="false" src="//d2kd9y1bp4zc6.cloudfront.net/?bydkd=947482"><\/script>`)
+    postscribe(
+      `#newm`,
+      `<script type="text/javascript">
+    atOptions = {
+        'key' : 'a764aa61a3b0a1d06d926ea1d02d94aa',
+        'format' : 'iframe',
+        'height' : 600,
+        'width' : 160,
+        'params' : {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://groundlesscobwebmiller.com/a764aa61a3b0a1d06d926ea1d02d94aa/invoke.js"></scr' + 'ipt>');
+<\/script>`
+    )
+        postscribe(
+      `#newm1`,
+      `<script type="text/javascript">
+    atOptions = {
+        'key' : 'ac58d42f55c934eb98864fea04bd6e07',
+        'format' : 'iframe',
+        'height' : 50,
+        'width' : 320,
+        'params' : {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://groundlesscobwebmiller.com/ac58d42f55c934eb98864fea04bd6e07/invoke.js"></scr' + 'ipt>');
+<\/script>`
+    )
+        postscribe(
+      `#newm2`,
+      `<script type="text/javascript">
+    atOptions = {
+        'key' : '77a101276fe7aeda49d94f3b505decc4',
+        'format' : 'iframe',
+        'height' : 300,
+        'width' : 160,
+        'params' : {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://groundlesscobwebmiller.com/77a101276fe7aeda49d94f3b505decc4/invoke.js"></scr' + 'ipt>');
+<\/script>`
+    )
     postscribe(
       '#dada',
       `<script type="text/javascript">
